52406c25411a9c33b20fd7ba5d37e04d8abfa505
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-27 16:12:38 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Quote from "./sections/Quote";
 import HeroSection from "./sections/HeroSection";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import PhoneModel from "./sections/PhoneModel";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Quote />
+      <PhoneModel />
       <HeroSection />
     </>
   );
---
+++ src/sections/PhoneModel.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Model } from '../assets/3D-Model/Scene'
+
+const Container = styled.div`
+      width: 100vw;
+      height: 100vh;
+
+      position: fixed;
+      top: 0;
+      z-index: 1;
+      background-color: transparent;
+`
+
+const PhoneModel = () => {
+  return (
+    <Container>
+      <Canvas>
+            <ambientLight intensity={1.25} />
+            <directionalLight position={[1, 0, 0]} />
+            <Model />
+            <OrbitControls />
+      </Canvas>
+    </Container>
+  )
+}
+
+export default PhoneModel
