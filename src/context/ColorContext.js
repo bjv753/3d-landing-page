480c7f3db48051a4f60b1a2ff1a054f4b848ac90
Benjamin Vredevelt <bjv753@gmail.com>
2023-11-10 15:31:06 -0600

    color context update

--- src/context/ColorContext.js
+++ src/context/ColorContext.js
@@ -3,3 +3,19 @@ import { createContext } from "react";
 
 
 export const ColorContext = createContext({})
+
+export const colorContetxProvider = ({children}) => {
+
+      const [currentColor, setCurrentColor] = useState({
+            color: "#9BB5CE",
+            text: "Sierra Blue",
+            rgbColor: "155, 181, 206"
+      })
+
+      return  (
+            <ColorContext.Provider value={{currentColor}}>
+                        {children}
+            </ColorContext.Provider>
+      )
+
+}
