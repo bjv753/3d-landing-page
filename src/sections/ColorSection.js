a3e7e43dd03a3de364a74d92f367fd629461692b
Benjamin Vredevelt <bjv753@gmail.com>
2023-11-16 08:42:36 -0600

    update

--- src/sections/ColorSection.js
+++ src/sections/ColorSection.js
@@ -1,12 +1,14 @@
-import React, { useRef, useLayoutEffect, Suspense, useContext, useEffect } from 'react';
-import { gsap } from "gsap";
-import styled from 'styled-components';
-import { useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { Model2 } from '../components/Scene2';
-import { ColorContext } from '../context/ColorContext';
-
-// Error is in ColorSection 
+/* eslint-disable react-hooks/exhaustive-deps */
+import gsap from "gsap";
+import React from "react";
+import { useRef, useLayoutEffect } from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Model2 from "../components/Scene2";
+import { useContext } from "react";
+import { ColorContext } from "./../context/ColorContext";
+import { useEffect } from "react";
 
 const Section = styled.section`
   width: 100vw;
@@ -52,6 +54,7 @@ const Center = styled.div`
   transform: translate(-50%, -50%) rotate(-90deg);
   font-size: var(--fontxxl);
   text-transform: uppercase;
+  filter: brightness(0.85);
 
   @media screen and (max-width: 48em) {
     top: 2rem;
@@ -60,127 +63,115 @@ const Center = styled.div`
 `;
 
 const ColorSection = () => {
+  const sectionRef = useRef(null);
+  const rightRef = useRef(null);
+  const leftRef = useRef(null);
+  const textRef = useRef(null);
 
-      const sectionRef= useRef(null);
-      const rightRef= useRef(null);
-      const leftRef= useRef(null);
-      const textRef= useRef(null);
+  const { currentColor, changeColorContext } = useContext(ColorContext);
 
+  useEffect(() => {
+    let rightElem = rightRef.current;
+    let leftElem = leftRef.current;
+    let textElem = textRef.current;
 
-      const { currentColor, changeColorContext } = useContext(ColorContext) 
+    textElem.innerText = currentColor.text;
+    textElem.style.color = currentColor.color;
 
-      useEffect(() => {
+    rightElem.style.backgroundColor = `rgba(${currentColor.rgbColor}, 0.4)`;
+    leftElem.style.backgroundColor = `rgba(${currentColor.rgbColor}, 0.8)`;
+  }, [currentColor]);
 
-            let rightElem = rightRef.current;
-            let leftElem = leftRef.current;
-            let textElem = textRef.current;
+  useLayoutEffect(() => {
+    let Elem = sectionRef.current;
 
-            textElem.innerText = currentColor.text;
-            textElem.style.color = currentColor.color;
+    let updateColor = (color, text, rgbColor) => {
+      const colorObj = {
+        color,
+        text,
+        rgbColor,
+      };
+      changeColorContext(colorObj);
+    };
 
-            rightElem.style.backgroundColor = `rgba(${currentColor.rgbColor}, 0.8)`
-            leftElem.style.backgroundColor = `rgba(${currentColor.rgbColor}, 0.8)`
-      
-      }, [currentColor])
-      
+    // pin the section
+    gsap.to(Elem, {
+      scrollTrigger: {
+        trigger: Elem,
+        start: "top top",
+        end: `+=${Elem.offsetWidth + 1000}`,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
 
-      useLayoutEffect(() => {
-            let Elem = sectionRef.current;
-            
+    let t2 = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: Elem,
+          start: "top top",
+          end: `+=${Elem.offsetWidth + 1000}`,
+          scrub: 1,
+        },
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#A50011", "Red", "165, 0, 17"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#A50011", "Red", "165, 0, 17"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#215E7C", "Blue", "33, 94, 124"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#215E7C", "Blue", "33, 94, 124"],
+      });
 
-            let updateColor = (color, text, rgbColor) => {
-
-                  const colorObj = {
-                        color,
-                        text,
-                        rgbColor,
-                  }
-                  changeColorContext(colorObj)
-
-
-            }
-
-            // pin the section 
-            gsap.to(Elem, {
-                        scrollTrigger: {
-                              trigger: Elem,
-                              start: "top top",
-                              end: `+=${Elem.offsetWidth +1000} `,
-                              scrub: true,
-                              pin: true,
-                              pinSpacing: true,
-                        }
-                  })
-
-            let t2 = gsap.timeline({
-                  scrollTrigger: {
-                        trigger: Elem,
-                        start: "top top",
-                        end: `+=${Elem.offsetWidth +1000} `,
-                        scrub: true,
-                        pinSpacer: true,
-                  }
-            }).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"]
-                        }
-                  ).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#F9E5C9", "Gold", "249, 229, 201"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#F9E5C9", "Gold", "249, 229, 201"]
-                        }
-                  ).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#505F4E", "Alpine Green", "80, 95, 78"]
-                        }
-                  ).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#574f6f", "Deep Purple", "87, 79, 111"]
-                        }
-                  ).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#A50011", "Red", "165, 0, 17"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#A50011", "Red", "165, 0, 17"]
-                        }
-                  ).to(Elem,
-                        {
-                              onStart: updateColor,
-                              onStartParams: ["#215E7C", "Blue", "33, 94, 124"],
-                              onReverseComplete: updateColor,
-                              onReverseCompleteParams: ["#215E7C", "Blue", "33, 94, 124"]
-                        }
-                  )
-
-      }, []);
+    return () => {
+      if (t2) t2.kill();
+    };
+  }, []);
 
   return (
-    <Section ref={sectionRef} >
-            <Left ref={leftRef} />
-            <Center ref={textRef} />
-            <Right ref={rightRef} >
-                  <Canvas camera={{ fov:6.5 }} >
-                  <ambientLight intensity={2.25} />
-                  <directionalLight intensity={0.4} />
-                  <Suspense fallback={null} >
-                  <Model2 />
-                  </Suspense>
-                  </Canvas>
-            </Right>
+    <Section ref={sectionRef}>
+      <Left ref={leftRef} />
+      <Center ref={textRef} />
+      <Right ref={rightRef}>
+        <Canvas camera={{ fov: 6.5 }}>
+          <ambientLight intensity={1.25} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model2 />
+          </Suspense>
+        </Canvas>
+      </Right>
     </Section>
-  )
-}
+  );
+};
 
-export default ColorSection
+export default ColorSection;
